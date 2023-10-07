73e21c3a0cff822a1b859bf6da652c5193dadbe2
Allen-Josu <allenalackaparambil@gmail.com>
2023-10-07 22:54:31 +0530

    Designed Login and Signup Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import Home from "./components/HomePage/Home";
+import "./app.css"
+
 function App() {
 
 
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,53 +1,74 @@
-import { Box,TextField,Button,Typography} from '@mui/material'
-import { Link} from "react-router-dom";
-import React from 'react'
-import Paper from '@mui/material/Paper/Paper'
+import { Box, TextField, Button, Typography, FormControl } from "@mui/material";
+import { Link } from "react-router-dom";
+import React from "react";
+import Paper from "@mui/material/Paper/Paper";
+import "./login.css";
 // import { useMutation } from "react-query";
 function Login() {
+  const [userName, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const [userName, setUserName] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  // const { mutate, isLoading } = useMutation(authenticate.loginUser, {
+  //     onSuccess: ({ data, status }) => {
+  //       console.log(data);
+  //       if (status === 201) {
+  //         alert("You are banned from anywhere");
+  //         return;
+  //       }
+  //       localStorage.setItem("anywhere-user", JSON.stringify(data));
+  //       setUser(data.username);
+  //       navigate("/");
+  //     },
+  //   });
 
-    // const { mutate, isLoading } = useMutation(authenticate.loginUser, {
-    //     onSuccess: ({ data, status }) => {
-    //       console.log(data);
-    //       if (status === 201) {
-    //         alert("You are banned from anywhere");
-    //         return;
-    //       }
-    //       localStorage.setItem("anywhere-user", JSON.stringify(data));
-    //       setUser(data.username);
-    //       navigate("/");
-    //     },
-    //   });
+  async function loginUser() {
+    const userDetails = {
+      userName: userName,
+      password: password,
+    };
 
-    async function loginUser() {
-        
-        const userDetails = {
-          userName:userName ,
-          password: password,
-        };
-    
-        if (userDetails.password.length < 8) {
-          alert("Incorrect Password");
-          return;
-        }
-    
-        // await mutate(userDetails);
-      }
+    if (userDetails.password.length < 8) {
+      alert("Incorrect Password");
+      return;
+    }
+
+    // await mutate(userDetails);
+  }
   return (
-    <Box component={Paper} sx={{height:"500px",width:"500px",marginLeft:"30%",marginTop:"5%"}}>
-   
-        <Box sx={{display:"flex",flexDirection:"column",gap:"3rem",width:"60%",marginLeft:"20%",paddingTop:"15%"}}>
-            <Typography sx={{fontWeight:"800",fontSize:"20px",paddingLeft:"25%"}}>Mentor Mentee</Typography>
-        <TextField onChange={(e:any)=>{setUserName(e.target.value)}} id="outlined-basic" label="Enter UserName" variant="outlined" />
-        <TextField onChange={(e:any)=>{setPassword(e.target.value)}} id="outlined-basic" label="Enter Password" variant="outlined" />
-        <Button onClick={loginUser}>Login</Button>  
-        </Box>
-       <Link to={"/signUp"}> <Typography sx={{fontSize:"10px",marginLeft:"43%"}}>Create Account </Typography></Link>
+    <div className="bg">
+      <Box component={Paper} elevation={15} className="login__border">
+        <h2 className="login__heading ">Welcome Back</h2>
+        <form action="">
+          <TextField
+            onChange={(e: any) => {
+              setUserName(e.target.value);
+            }}
+            className="login__textfield"
+            id="outlined-basic"
+            label="Enter Username"
+            variant="outlined"
+          />
+          <TextField
+            onChange={(e: any) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            id="outlined-basic"
+            className="login__textfield"
+            label="Enter Password"
+            variant="outlined"
+          />
+          <Button className="login__button" variant="contained" size="large" onClick={loginUser}>Login</Button>
+        </form>
+        <div className="login__signup">
+          <Link to={''} className="login__link"><p className="login__passreset">Reset your Password</p></Link>
+          <Link to={'/signUp'} className="login__link"> <p> Signup Now</p></Link>
+        </div>
+      </Box>
+    </div>
 
-    </Box>
-  )
+
+  );
 }
 
-export default Login
+export default Login;
